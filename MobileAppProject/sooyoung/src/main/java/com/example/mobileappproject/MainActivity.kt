2eec2c820607ac048cc75f2a74b182b12488f5ad
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2022-11-07 00:15:34 +0900

    timerUI

--- MobileAppProject/sooyoung/src/main/java/com/example/mobileappproject/MainActivity.kt
+++ MobileAppProject/sooyoung/src/main/java/com/example/mobileappproject/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.mobileappproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mobileappproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var initTime = 0L
+    var pauseTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //TIMER
+        binding.startBtn.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
+        }
+
+        binding.stopBtn.setOnClickListener {
+            binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+        }
+
+        binding.resetBtn.setOnClickListener {
+            pauseTime = 0L
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+        }
+
+        // LIST
+        val datas = mutableListOf<String>()
+        for(i in 1..5) {
+            datas.add("timer $i")
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = recyclerAdapter(datas)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
     }
 }
